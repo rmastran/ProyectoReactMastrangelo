97fbf4a7ecabd14e0c50ee7d4efd1c4301635799
rmastran <taquepelacontacto@gmail.com>
2023-07-27 23:30:37 -0300

    Ajustando detalles para pre entrega 2

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -6,10 +6,9 @@ import { useParams } from 'react-router-dom';
 const ItemListContainer = ({greeting = "Ofertas"}) => {
 
     const [dataPelotas, setDataPelotas] = useState([]);
-    const [loading, setLoading] = useState(false);
     const {categoryId}= useParams()
     useEffect(() => {
-        setLoading(true)
+
         getDataPelotas.then(res => {
             if(categoryId){
                 setDataPelotas(res.filter((dataPelotas)=> dataPelotas.category === categoryId))
@@ -17,16 +16,21 @@ const ItemListContainer = ({greeting = "Ofertas"}) => {
                 setDataPelotas(res)
             }
         }) 
-       .finally(()=> setLoading(false))
     }, [categoryId])
 
-    if (loading){
-        return <div>Cargando...</div>
-    }
     return (
         <div className='d-flex flex-column align-items-center'>
+            {  dataPelotas.length == 0 
+            ?
+            <h1>Cargando..</h1>
+            :
+            <>
             <h2 style={{backgroundColor: "coral", textAlign: "center"}}>{greeting} <span>{categoryId && categoryId}</span></h2>
             <ItemList dataPelotas={dataPelotas} />
+            </>
+            
+            }
+            
         </div>
                 
     );
